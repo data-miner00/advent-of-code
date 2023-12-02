df74d87828af4a16a24b852c4792ec59a61dfd6f
data-miner00 <data-miner00@users.noreply.github.com>
2023-12-02 15:50:23 +0800

    day 2

---
+++ day-02/src/part1.rs
@@ -0,0 +1,69 @@
+use regex::Regex;
+
+fn process_game(game: &str) -> (i32, i32, i32, i32) {
+    let id_re = Regex::new(r"\d+").unwrap();
+    let blue_re = Regex::new(r"(?<blue>\d+) blue").unwrap();
+    let green_re = Regex::new(r"(?<green>\d+) green").unwrap();
+    let red_re = Regex::new(r"(?<red>\d+) red").unwrap();
+
+    let splitted = game.split(';');
+
+    let id_cap = id_re.captures(game).unwrap();
+    let id: i32 = id_cap.get(0).unwrap().as_str().parse().unwrap_or_default();
+    let mut blue_max = 0;
+    let mut green_max = 0;
+    let mut red_max = 0;
+
+    splitted.for_each(|case| {
+        let blue = if let Some(blue_cap) = blue_re.captures(case) {
+            blue_cap["blue"].parse().unwrap_or_default()
+        } else { 0 };
+
+        let green = if let Some(green_cap) = green_re.captures(case) {
+            green_cap["green"].parse().unwrap_or_default()
+        } else { 0 };
+
+        let red = if let Some(red_cap) = red_re.captures(case) {
+            red_cap["red"].parse().unwrap_or_default()
+        } else { 0 };
+
+        if blue > blue_max {
+            blue_max = blue;
+        }
+
+        if green > green_max {
+            green_max = green;
+        }
+
+        if red > red_max {
+            red_max = red;
+        }
+    });
+
+    (id, blue_max, green_max, red_max)
+}
+
+pub fn process(input: &str) -> i32 {
+    let lines = input.lines();
+
+    let output = lines.map(process_game);
+
+    let red = 12;
+    let green = 13;
+    let blue = 14;
+
+    let possible = output.filter(|(_, b, g, r)| *r <= red && *b <= blue && *g <= green);
+    possible.fold(0, |acc, (id, _, _, _)| acc + id)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> Result<(), Box<dyn std::error::Error>> {
+        let input = include_str!("demo.txt");
+        assert_eq!(8, process(input));
+        Ok(())
+    }
+}
---
+++ day-02/src/part2.rs
@@ -0,0 +1,61 @@
+use regex::Regex;
+
+fn process_game(game: &str) -> i32 {
+    let blue_re = Regex::new(r"(?<blue>\d+) blue").unwrap();
+    let green_re = Regex::new(r"(?<green>\d+) green").unwrap();
+    let red_re = Regex::new(r"(?<red>\d+) red").unwrap();
+
+    let splitted = game.split(';');
+
+    let mut blue_min = 0;
+    let mut green_min = 0;
+    let mut red_min = 0;
+
+    splitted.for_each(|case| {
+        let blue = if let Some(blue_cap) = blue_re.captures(case) {
+            blue_cap["blue"].parse().unwrap_or_default()
+        } else { 0 };
+
+        let green = if let Some(green_cap) = green_re.captures(case) {
+            green_cap["green"].parse().unwrap_or_default()
+        } else { 0 };
+
+        let red = if let Some(red_cap) = red_re.captures(case) {
+            red_cap["red"].parse().unwrap_or_default()
+        } else { 0 };
+
+        if blue > blue_min {
+            blue_min = blue;
+        }
+
+        if green > green_min {
+            green_min = green;
+        }
+
+        if red > red_min {
+            red_min = red;
+        }
+    });
+
+    blue_min * green_min * red_min
+}
+
+pub fn process(input: &str) -> i32 {
+    let lines = input.lines();
+
+    let result = lines.map(process_game).sum();
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> Result<(), Box<dyn std::error::Error>> {
+        let input = include_str!("demo.txt");
+        assert_eq!(2286, process(input));
+        Ok(())
+    }
+}
